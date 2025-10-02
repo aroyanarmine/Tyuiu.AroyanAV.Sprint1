3ece56f119cc275b7412058e271becf9fd9d2bfd
aroyanarmine <aroanarmine068@gmail.com>
2025-10-03 00:15:04 +0500

    Task2

---
+++ Tyuiu.AroyanAV.Sprint1.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AroyanAV.Sprint1.Task2.V26.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint1.Task2.V26.Test;
+
+[TestClass]
+public sealed class DataServiceTest
+{
+    [TestMethod]
+    public void CalendarCheck()
+    {
+        DataService ds = new DataService();
+        int value = 7;
+        int valueTwo = 15;
+        var res = ds.CalculateMinutesSinceStart(value, valueTwo);
+        Assert.AreEqual(435, res);
+    }
+}
---
+++ Tyuiu.AroyanAV.Sprint1.Task2.V26/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AroyanAV.Sprint1.Task2.V26.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint1.Task2.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Ароян А. В. | ИСТНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнила: Ароян Армине Вачагановна | ИСТНб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int value, valueTwo;
+
+            Console.WriteLine("Введите часы:");
+            value = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите минуты:");
+            valueTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CalculateMinutesSinceStart(value, valueTwo));
+
+            Console.ReadLine();
+        }
+    }
+}
