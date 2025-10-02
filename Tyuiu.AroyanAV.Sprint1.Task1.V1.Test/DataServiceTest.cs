6657fa3d8e7f42498823f18cc688b5661be39d4b
aroyanarmine <aroanarmine068@gmail.com>
2025-10-02 22:10:50 +0500

    Task1

---
+++ Tyuiu.AroyanAV.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AroyanAV.Sprint1.Task1.V1.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new DataService();
+            double a = 1;
+            double x = 27;
+            double y = 3;
+            var res = ds.Calculate(a, x, y);
+
+            Assert.AreEqual(9, res);
+        }
+    }
+}
