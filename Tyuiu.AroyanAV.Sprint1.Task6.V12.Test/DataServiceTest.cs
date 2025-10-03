88c496cf8059e4bed9786e75c8086d562dc129f7
aroyanarmine <aroanarmine068@gmail.com>
2025-10-03 22:35:40 +0500

    Task6

---
+++ Tyuiu.AroyanAV.Sprint1.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AroyanAV.Sprint1.Task6.V12.Lib;
+
+namespace Tyuiu.AroyaAV.Sprint1.Task6.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValiExpression()
+        {
+            DataService ds = new DataService();
+            string value = "солнце светит яркое солнце";
+            var res = ds.CheckLastWordRepetiton(value);
+            Assert.AreEqual(res, true);
+        }
+    }
+}
---
+++ Tyuiu.AroyanAV.Sprint1.Task6.V12/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.AroyanAV.Sprint1.Task6.V12.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint1.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Ароян А. В. | ИСТНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнила: Ароян Армине Вачагановна | ИСТНб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст.                          *");
+            Console.WriteLine("* Проверить, что последнее слово строки входит в нее еще раз.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value;
+
+            Console.WriteLine("Введите строку:");
+            value = Convert.ToString(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Результат " + ds.CheckLastWordRepetiton(value));
+            Console.ReadLine();
+        }
+    }
+}
